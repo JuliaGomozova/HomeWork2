6a7d7d5fe8eb5ab4f0a697d32d577b8bd75d0b67
Julia Gomozova <sunrain@yandex.ru>
2022-12-05 20:55:23 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+
+string dayStr = Console.ReadLine() ??"";
+
+bool isNum = int.TryParse(dayStr, out int day);
+
+if (!isNum) 
+{
+    Console.WriteLine("Надо ввести номер дня недели");
+    return;
+}
+
+//int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7)
+    Console.WriteLine("Нет такого дня недели");
+else
+{
+    if (day == 6 || day == 7)
+        Console.WriteLine($"День {day} является выходным");
+    else 
+        Console.WriteLine($"День {day} не является выходным");
+}    
+
