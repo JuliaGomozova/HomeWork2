f672272821e102efae7d4666ee94eac61586c377
Julia Gomozova <sunrain@yandex.ru>
2022-12-05 20:39:33 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число:");
+
+string numStr = Console.ReadLine() ??"";
+
+bool isNum = int.TryParse(numStr, out int num);
+
+if (!isNum) 
+{
+    Console.WriteLine("Вы ввели не число");
+    return;
+}
+
+num = Math.Abs(num);
+if (num < 100)
+    Console.WriteLine("в веденном числе нет третьей цифры");    
+else
+    Console.WriteLine($"Третья цифра во введенном числе = {num.ToString()[2]}");
