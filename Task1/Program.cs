1d31c9261e4282870a17c7e0f973cddbc210bef4
Julia Gomozova <sunrain@yandex.ru>
2022-12-05 20:17:36 +0300

    add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+
+string numStr = Console.ReadLine() ??"";
+
+bool isNum = int.TryParse(numStr, out int num);
+
+if (!isNum) 
+{
+    Console.WriteLine("Вы ввели не число");
+    return;
+}
+
+num = Math.Abs(num);
+if (num < 100 || num > 999)
+    Console.WriteLine("Введенное число не является трехзначным");    
+else
+    Console.WriteLine($"Вторая цифра во введенном числе = {num.ToString()[1]}");
